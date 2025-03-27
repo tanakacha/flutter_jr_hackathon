ba63da27a9e41751a05d3bad604d423456a5b966
tanakacha <kntasihc20.18@gmail.com>
2025-03-27 15:08:28 +0900

    feature(Flutter): アラーム設定時刻に通知を送信する機能の追加 #11

--- lib/services/notification.dart
+++ lib/services/notification.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart';
 
@@ -7,10 +8,10 @@ class Notifications {
   }
 
   // まだ使ってない
-  // static const _iosCategoryId = 'sample_category';
+  static const _iosCategoryId = 'sample_category';
   // static final _log = Logger('Notifications');
 
-  // final _plugin = FlutterLocalNotificationsPlugin();
+  final _plugin = FlutterLocalNotificationsPlugin();
   // final _initCompleter = Completer<void>();
 
   // 途中
@@ -18,5 +19,29 @@ class Notifications {
     tz.initializeTimeZones();
     setLocalLocation(getLocation('Asia/Tokyo'));
 
+    final success = await _plugin.initialize(
+      InitializationSettings(
+        iOS: DarwinInitializationSettings(
+          notificationCategories: [
+            DarwinNotificationCategory(
+              _iosCategoryId,
+              actions: [
+                DarwinNotificationAction.plain(
+                  'sample_action',
+                  'Sample Action',
+                  options: {DarwinNotificationActionOption.foreground},
+                ),
+              ],
+              options: {
+                DarwinNotificationCategoryOption.hiddenPreviewShowTitle,
+                DarwinNotificationCategoryOption.hiddenPreviewShowSubtitle,
+                DarwinNotificationCategoryOption.allowAnnouncement,
+              },
+            )
+          ],
+        ),
+        android: const AndroidInitializationSettings('@mipmap/ic_launcher'),
+      ),
+    );
   }
 }
