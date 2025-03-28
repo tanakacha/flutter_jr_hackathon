0636db20da85ca9b39783a789f2e9ae2c078f905
tanakacha <kntasihc20.18@gmail.com>
2025-03-28 23:39:05 +0900

    feature(Flutter): 起床タイマーの画面表示 #26

--- lib/scenes/check/check_screen.dart
+++ lib/scenes/check/check_screen.dart
@@ -1,29 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_jr_hackathon/provider/wake_up_time_provider.dart';
 import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CheckScreen extends StatelessWidget {
+class CheckScreen extends ConsumerStatefulWidget {
   const CheckScreen({super.key});
 
   @override
+  ConsumerState<CheckScreen> createState() => _CheckScreenState();
+}
+
+class _CheckScreenState extends ConsumerState<CheckScreen> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(wakeUpTimeNotifierProvider.notifier).startTimer();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final wakeUpTime = ref.watch(wakeUpTimeNotifierProvider);
+    final WakeUpMinutes =
+        ref.watch(wakeUpTimeNotifierProvider.notifier).minutes;
+    final WakeUpSeconds =
+        ref.watch(wakeUpTimeNotifierProvider.notifier).seconds;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('確認画面'),
       ),
       body: Column(
-        // mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           Gap(80),
           Text(
             '体を起こしましたか？',
             style: TextStyle(fontSize: 30),
           ),
+          Gap(20),
+          Text(
+            '${WakeUpMinutes.toString().padLeft(2, '0')}:${WakeUpSeconds.toString().padLeft(2, '0')}',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
           Gap(150),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              Gap(10),
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
                   elevation: 0,
@@ -31,14 +53,16 @@ class CheckScreen extends StatelessWidget {
                 child: Text(
                   'はい',
                   style: TextStyle(
-                    fontSize: 10,
+                    fontSize: 20,
                   ),
                 ),
                 onPressed: () {
+                  ref.read(wakeUpTimeNotifierProvider.notifier).stopTimer();
                   context.go('/game');
+                  print('timer');
+                  print(wakeUpTime);
                 },
               ),
-              Gap(0),
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
                   elevation: 0,
@@ -51,13 +75,11 @@ class CheckScreen extends StatelessWidget {
                   'いいえ',
                   style: TextStyle(
                     color: Colors.black,
-                    // color: Colors.blue,
                     fontSize: 30,
                   ),
                 ),
                 onPressed: () {},
               ),
-              Gap(0),
             ],
           ),
         ],
